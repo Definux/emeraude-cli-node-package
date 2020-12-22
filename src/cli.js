c4718bdd79d5d05743fcd6e267ccb64737fc3491
gsk567 <georgi.karagogov@gmail.com>
2020-12-23 01:19:21 +0200

    minor improvement of console

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
 const yargs = require('yargs');
 const { executeCliCommand } = require("./commandsFactory");
+const readJson = require('read-package-json');
+const path = require('path');
+const packageRoot = path.join(__dirname, '../package.json');
 
 function parseArgumentsIntoOptions(rawArgs) {
     const argv = yargs
@@ -20,6 +23,10 @@ module.exports = {
     cli: function (args) {
         let options = parseArgumentsIntoOptions(args);
         executeCliCommand(options);
+
+        readJson(packageRoot, console.error, false, (error, data) => {
+            console.log('EmPack Version: ' + data.version);
+        });
         console.log('- - - - - - - - - - - - - - - - ');
         console.log('Emeraude (https://emeraude.dev/)');
     }
